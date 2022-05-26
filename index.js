ee9354da4132b1f5025ef8843d9dc03532520b8f
Mary-Frye <deimospho@gmail.com>
2022-05-26 15:26:09 -0400

    final commit minus working grass and sky

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //Background//
-function background(url, bottom, height, width) {
+function tile(url, bottom, height, width) {
     for (let h = 0; h < height; h++) {
         for (let w = 0; w < width; w++) {
             newImage(url, left + w * 100, bottom + h * 100)
         }
     }
 }
-
 
 //Image duplication Code//
 function newImage(url, left, bottom) {
@@ -21,11 +20,11 @@ function newImage(url, left, bottom) {
 
 //New Item Duplication Code//
 function newItem(url, left, bottom) {
-    let object = newImage(url, left, bottom){
-        object.addEventListener('dblclick', () => {
-            object.remove()
-        })
-}
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
 }
 
 //Background Display//
@@ -33,24 +32,20 @@ let horizon = window.innerHeight / 1.75
 let skyHeight = window.innerHeight - horizon
 let grassHeight = horizon
 
-background('assets/skyHeight.png', 0, horizon, window.innerWidth / 100, skyHeight / 100)
-background('assets/grass.png', 0, 0, window.innerWidth / 100, grassHeight / 100)
+tile('assets/skyHeight.png', 0, 0, horizon, window.innerWidth / 100, skyHeight / 100)
+tile('assets/grass.png', 0, 0, window.innerWidth / 100, grassHeight / 100)
 
 newImage('assets/well.png', 500, 425)
 newImage('assets/tree.png', 200, 300)
-newImage('assets/red-character.gif', 250, 350)
 newImage('assets/pine-tree.png', 250, 350)
 newImage('assets/pillar.png', 350, 100)
-newImage('assets/log.png', 250, 350)
-newImage('assets/green-charcter.gif', 250, 350)
-newImage('assets/log.png', 250, 350)
-newImage('assets/boulder.png', 250, 350)
 newImage('assets/crate.png', 150, 200)
+newImage('assets/green-character.gif', 100, 100)
 
 newItem('assets/sword.png', 160, 180)
 newItem('assets/staff.png', 600, 100)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/rock.png', 160, 180)
+newItem('assets/sheild.png', 165, 185)
+
 
 //let greenCharacter = document.createElement('img')
 //greenCharacter.src = 'assets/green-character.gif'
@@ -65,4 +60,15 @@ newItem('assets/rock.png', 160, 180)
 //pineTree.style.left = '450px'
 //pineTree.style.bottom = '200px'
 //document.body.append(pineTree)
+
+//let sword = document.createElement('img')
+//sword.src = 'assets/sword.png'
+//sword.position = 'fixed'
+//sword.left = '500px'
+//sword.bottom = '405px'
+//document.body.append(sword)
+
+//sword.addEventListener('click', function () {
+  //  sword.remove()
+//})
 
